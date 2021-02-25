32effd8dc5a6aec609ea1eb053d3240e3b443cb9
gameswithtony <76749340+gameswithtony@users.noreply.github.com>
2021-02-24 23:41:54 -0500

    Initial Eliza

---
+++ eliza/data.js
@@ -0,0 +1,121 @@
+const LOCALSTORAGENAME = 'elizagamestate';
+const ERA = { Canal: 0, Rail: 1 };
+const INDUSTRY = { Brewery: 0, Manufacturer: 1, CottonMill: 2, Pottery: 3, CoalMine: 4, IronWorks: 5 };
+const LOCATIONTYPE = { Industries: 0, Merchants: 1 };
+const BONUSTYPE = { Pounds: 0, VPs: 1 };
+
+// ids on industry spots used for reading order
+// adjacency listed in clockface order
+const INITIAL_BOARD = {
+    locations: [
+        {
+            id: 0,
+            type: LOCATIONTYPE.Merchants,
+            name: "Warrington",
+            bonus: 5,
+            bonusType: BONUSTYPE.Pounds,
+            spaces: [
+                {
+                    id: 0,
+                    totalBeer: 0,
+                    tile: null
+                },
+                {
+                    id: 1,
+                    totalBeer: 0,
+                    tile: null
+                }
+            ],
+            adjacentToCanal: [1],
+            adjacentToRail: [1]
+        },
+        {
+            id: 1,
+            type: LOCATIONTYPE.Industries,
+            name: "Stoke-On-Trent",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Manufacturer
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.Pottery,
+                        INDUSTRY.IronWorks
+                    ],
+                    tile: null
+                },
+                {
+                    id: 2,
+                    types: [
+                        INDUSTRY.Manufacturer
+                    ],
+                    tile: null
+                }
+            ],
+            adjacentToCanal: [0,2],
+            adjacentToRail: [0,2]
+        },
+        {
+            id: 2,
+            type: LOCATIONTYPE.Industries,
+            name: "Leek",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Manufacturer
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.CoalMine
+                    ],
+                    tile: null
+                }
+            ],
+            adjacentToCanal: [1,3],
+            adjacentToRail: [1,3]
+        },
+        {
+            id: 3,
+            type: LOCATIONTYPE.Industries,
+            name: "Belper",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Manufacturer
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.CoalMine
+                    ],
+                    tile: null
+                },
+                {
+                    id: 2,
+                    types: [
+                        INDUSTRY.Pottery
+                    ],
+                    tile: null
+                }
+            ],
+            adjacentToCanal: [2,7],
+            adjacentToRail: [2,7]
+        }
+    ]
+};
