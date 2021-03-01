25728072201e8ad072d5f86e830c824c35d7a2df
Anthony P. Alicea <preneer@gmail.com>
2021-03-01 18:38:58 -0500

    Update data.js

--- eliza/data.js
+++ eliza/data.js
@@ -2,7 +2,7 @@ const LOCALSTORAGENAME = 'elizagamestate';
 const ERA = { Canal: 0, Rail: 1 };
 const INDUSTRY = { Brewery: 0, Manufacturer: 1, CottonMill: 2, Pottery: 3, CoalMine: 4, IronWorks: 5 };
 const LOCATIONTYPE = { Industries: 0, Merchants: 1 };
-const BONUSTYPE = { Pounds: 0, VPs: 1 };
+const BONUSTYPE = { Pounds: 0, VPs: 1, Income: 2, Develop: 3 };
 
 // ids on industry spots used for reading order
 // adjacency listed in clockface order
@@ -13,7 +13,7 @@ const INITIAL_BOARD = {
             type: LOCATIONTYPE.Merchants,
             name: "Warrington",
             bonus: 5,
-            bonusType: BONUSTYPE.Pounds,
+            bonusType: BONUSTYPE.VPs,
             spaces: [
                 {
                     id: 0,
@@ -177,6 +177,189 @@ const INITIAL_BOARD = {
                 },
                 {
                     toId: 7,
+                    tile: null
+                }
+            ]
+        },
+        {
+            id: 4,
+            type: LOCATIONTYPE.Industries,
+            name: "Stone",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Brewery
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.Manufacturer,
+                        INDUSTRY.CoalMine
+                    ],
+                    tile: null
+                }
+            ],
+            edgesCanal: [
+                {
+                    toId: 1,
+                    tile: null
+                },
+                {
+                    toId: 9,
+                    tile: null
+                },
+                {
+                    toId: 8,
+                    tile: null
+                }
+            ],
+            edgesRail: [
+                {
+                    toId: 1,
+                    tile: null
+                },
+                {
+                    toId: 5,
+                    tile: null
+                },
+                {
+                    toId: 9,
+                    tile: null
+                },
+                {
+                    toId: 8,
+                    tile: null
+                }
+            ]
+        },
+        {
+            id: 5,
+            type: LOCATIONTYPE.Industries,
+            name: "Uttoxeter",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.Manufacturer,
+                        INDUSTRY.Brewery
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Brewery
+                    ],
+                    tile: null
+                }
+            ],
+            edgesCanal: [
+            ],
+            edgesRail: [
+                {
+                    toId: 6,
+                    tile: null
+                },
+                {
+                    toId: 4,
+                    tile: null
+                }
+            ]
+        },
+        {
+            id: 6,
+            type: LOCATIONTYPE.Industries,
+            name: "Derby",
+            spaces: [
+                {
+                    id: 0,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Brewery
+                    ],
+                    tile: null
+                },
+                {
+                    id: 1,
+                    types: [
+                        INDUSTRY.CottonMill,
+                        INDUSTRY.Manufacturer
+                    ],
+                    tile: null
+                },
+                {
+                    id: 2,
+                    types: [
+                        INDUSTRY.IronWorks
+                    ],
+                    tile: null
+                }
+            ],
+            edgesCanal: [
+                {
+                    toId: 3,
+                    tile: null
+                },
+                {
+                    toId: 7,
+                    tile: null
+                },
+                {
+                    toId: 9,
+                    tile: null
+                }
+            ],
+            edgesRail: [
+                {
+                    toId: 3,
+                    tile: null
+                },
+                {
+                    toId: 5,
+                    tile: null
+                },
+                {
+                    toId: 7,
+                    tile: null
+                },
+                {
+                    toId: 9,
+                    tile: null
+                }
+            ]
+        },
+        {
+            id: 7,
+            type: LOCATIONTYPE.Merchants,
+            name: "Nottingham",
+            bonus: 3,
+            bonusType: BONUSTYPE.VPs,
+            spaces: [
+                {
+                    id: 0,
+                    totalBeer: 0,
+                    tile: null
+                },
+                {
+                    id: 1,
+                    totalBeer: 0,
+                    tile: null
+                }
+            ],
+            edgesCanal: [
+                {
+                    toId: 6,
+                    tile: null
+                }
+            ],
+            edgesRail: [
+                {
+                    toId: 6,
                     tile: null
                 }
             ]
