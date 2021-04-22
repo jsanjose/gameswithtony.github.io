8afcc7e6f31d5efeee33f3978ce2074007383cc4
gameswithtony <76749340+gameswithtony@users.noreply.github.com>
2021-04-22 15:48:24 -0400

    Prep for future deck types

--- eliza/data.js
+++ eliza/data.js
@@ -1472,6 +1472,7 @@ const CARDS = [
 
 const AI_DECK_TYPES = { Balanced: 0, Birmingham: 1 };
 function getAIDeck(type, numberOfPlayers) {
+    // Balanced
     if (type === AI_DECK_TYPES.Balanced && numberOfPlayers == '2') {
         return [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 24];
     }
@@ -1480,6 +1481,8 @@ function getAIDeck(type, numberOfPlayers) {
         // removes Cannock, Tamworth, and Dudley
         return [3, 4, 5, 6, 7, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
     }
+
+    // 
 }
 
 const TOTAL_ROUNDS_2PLAYER = 10;
