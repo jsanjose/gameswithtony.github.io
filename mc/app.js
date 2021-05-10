815e3bcec4e31c31ed981089902fe72a7f2396c6
gameswithtony <76749340+gameswithtony@users.noreply.github.com>
2021-05-09 23:20:54 -0400

    Complete basic functionality

--- mc/app.js
+++ mc/app.js
@@ -1,12 +1,15 @@
 const LOCALSTORAGENAME = "mcgamestate";
 
+const updateHitPoints = function (points) {
+    this.hitpoints = this.hitpoints + points;
+    app.saveGameState();
+}
+
 function createCharacter(name, hitpoints) {
     return {
         name: name,
         hitpoints: hitpoints,
-        updateHitPoints: function (points) {
-            this.hitpoints = this.hitpoints + points;
-        }
+        updateHitPoints: updateHitPoints
     };
 }
 
@@ -16,24 +19,45 @@ var app = new Vue({
         characters: [
             createCharacter('Villian', 14),
             createCharacter('Hero', 10)
-        ]
+        ],
+        isEditing: false
     },
     mounted: function() {
         this.computedUpdater++;
         if (localStorage.getItem(LOCALSTORAGENAME)) {
             let gameState = JSON.parse(localStorage.getItem(LOCALSTORAGENAME));
             this.characters = gameState.characters;
+
+            for(let i=0; i < this.characters.length; i++) {
+                this.characters[i].updateHitPoints = updateHitPoints;
+            };
         }
     },
     computed: {
         
     },
     methods: {
+        edit: function () {
+            this.isEditing = true;
+        },
+        save: function () {
+            this.isEditing = false;
+            this.saveGameState();
+        },
+        add: function () {
+            this.characters.push(createCharacter('', 10));
+        },
+        remove: function (index) {
+            if (confirm('Are you sure you want to remove this character?')) {
+                this.characters.splice(index, 1);
+            }
+        },
         reset: function() {
             this.characters = [
                 createCharacter('Villian', 14),
                 createCharacter('Hero', 10)
             ];
+            this.isEditing = false;
             this.saveGameState();
         },
         saveGameState: function() {
