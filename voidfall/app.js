94e6534186262707230b5c1ef9861b7d54059711
Anthony P. Alicea <preneer@gmail.com>
2023-09-22 21:04:12 -0400

    Visual refinements

--- voidfall/app.js
+++ voidfall/app.js
@@ -1334,7 +1334,7 @@ createApp({
         showResults: false,
         expandAll: true,
         computedUpdater: 1,
-        version: "0.50"
+        version: "0.51"
     } },
     watch: {
         numberOfPlayers(val) {
